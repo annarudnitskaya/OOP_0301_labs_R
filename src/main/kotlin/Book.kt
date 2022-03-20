fe297f12139f272763f9ba2dd78c5d6a6fe9d03b
Анна Рудницкая <anjarudnitckaia@gmail.com>
2022-03-20 15:18:37 +0300

    ...

--- src/main/kotlin/Book.kt
+++ src/main/kotlin/Book.kt
@@ -1,10 +1,12 @@
 package lab1
 
-data class Book( val name: String, val authors: String, val yearBook: Int) {}
+data class Book(val name: String, val authors: String, val yearBook: Int) {}
 
 //parser of books
 fun parserBooks(books: String): List<Book>? {
-    if (books.isEmpty()) { return null }
+    if (books.isEmpty()) {
+        return null
+    }
     val splitString = books.split('\n')
     val listBooks = mutableListOf<Book>()
     var perem: String
@@ -16,11 +18,12 @@ fun parserBooks(books: String): List<Book>? {
         val author = perem.substringBeforeLast("//").trim()
         perem = perem.substringAfter("//").trim()
         val yearBook = perem.toInt()
-        val fullBook = Book( name, author, yearBook)
+        val fullBook = Book(name, author, yearBook)
         listBooks.add(fullBook)
     }
     return listBooks
 }
+
 //functions of processing of books
 fun oldestBook(books: List<Book>): Book? = books.minByOrNull { it.yearBook }
 fun youngestBook(books: List<Book>): Book? = books.maxByOrNull { it.yearBook }
