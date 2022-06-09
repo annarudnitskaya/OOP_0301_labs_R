8a43ef6103032641fd2821c115ce1065d522c9c8
Alina <al.litovckina@yandex.ru>
2022-06-09 14:16:50 +0300

    courswork for cvFiles

--- src/main/kotlin/Book.kt
+++ src/main/kotlin/Book.kt
@@ -1,41 +1,41 @@
-//package lab1
-//
-//data class Book(val name: String, val authors: String, val yearBook: Int) {}
-//
-////parser of books
-//fun parserBooks(books: String): List<Book>? {
-//    if (books.isEmpty()) return null
-//    val splitString = books.split('\n')
-//    val listBooks = mutableListOf<Book>()
-//    var perem: String
-//    for (str in splitString) {
-//        perem = str
-//        perem = perem.substringAfter('.')
-//        val name = perem.substringBefore("//").trim()
-//        if (name.isEmpty()) {
-//            throw IllegalArgumentException("One book has no title")
-//        }
-//        perem = perem.substringAfter("//")
-//        val author = perem.substringBeforeLast("//").trim()
-//        if (author.isEmpty()) {
-//            throw IllegalArgumentException("One book has no author")
-//        }
-//        perem = perem.substringAfter("//").trim()
-//        if (perem.isEmpty()) {
-//            throw IllegalArgumentException("One book has no year")
-//        }
-//        val yearBook = perem.toInt()
-//        if (yearBook < 1) {
-//            throw IllegalArgumentException("One book has incorrect year")
-//        }
-//        val fullBook = Book(name, author, yearBook)
-//        listBooks.add(fullBook)
-//    }
-//    return listBooks
-//}
-//
-////functions of processing of books
-//fun oldestBook(books: List<Book>): Book? = books.minByOrNull { it.yearBook }
-//fun youngestBook(books: List<Book>): Book? = books.maxByOrNull { it.yearBook }
-//fun longestNameBook(books: List<Book>): Book? = books.maxByOrNull { it.name.length }
-//fun shortestNameBook(books: List<Book>): Book? = books.minByOrNull { it.name.length }
+package lab1
+
+data class Book(val name: String, val authors: String, val yearBook: Int) {}
+
+//parser of books
+fun parserBooks(books: String): List<Book>? {
+    if (books.isEmpty()) return null
+    val splitString = books.split('\n')
+    val listBooks = mutableListOf<Book>()
+    var perem: String
+    for (str in splitString) {
+        perem = str
+        perem = perem.substringAfter('.')
+        val name = perem.substringBefore("//").trim()
+        if (name.isEmpty()) {
+            throw IllegalArgumentException("One book has no title")
+        }
+        perem = perem.substringAfter("//")
+        val author = perem.substringBeforeLast("//").trim()
+        if (author.isEmpty()) {
+            throw IllegalArgumentException("One book has no author")
+        }
+        perem = perem.substringAfter("//").trim()
+        if (perem.isEmpty()) {
+            throw IllegalArgumentException("One book has no year")
+        }
+        val yearBook = perem.toInt()
+        if (yearBook < 1) {
+            throw IllegalArgumentException("One book has incorrect year")
+        }
+        val fullBook = Book(name, author, yearBook)
+        listBooks.add(fullBook)
+    }
+    return listBooks
+}
+
+//functions of processing of books
+fun oldestBook(books: List<Book>): Book? = books.minByOrNull { it.yearBook }
+fun youngestBook(books: List<Book>): Book? = books.maxByOrNull { it.yearBook }
+fun longestNameBook(books: List<Book>): Book? = books.maxByOrNull { it.name.length }
+fun shortestNameBook(books: List<Book>): Book? = books.minByOrNull { it.name.length }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,32 @@
-//package lab1
-//
-//fun main() {
-//    val books = """1. Shadow and Bone // Leigh Bardugo // 2012
-//        2. City of Bones (Mortal Instruments) // Cassandra Clare // 2007
-//        3. Ninth House // Leigh Bardugo // 2019
-//        4. Dune // Franklin Patrick Herbert // 1965
-//        5. Roadside Picnic // Arcady and Boris Strugataky // 1972"""
-//    val listBooks: List<Book>? = parserBooks(books)
-//    if (oldestBook(listBooks!!) != null) println("The one of the oldest book was published: ${oldestBook(listBooks)}")
-//    if (youngestBook(listBooks) != null) println(
-//        "The one of the youngest book was published: ${
-//            youngestBook(
-//                listBooks
-//            )
-//        }"
-//    )
-//    if (longestNameBook(listBooks) != null) println(
-//        "The one of the longest name of the book is: ${
-//            longestNameBook(
-//                listBooks
-//            )
-//        }"
-//    )
-//    if (shortestNameBook(listBooks) != null) println(
-//        "The one of the shortest name of the book is: ${
-//            shortestNameBook(
-//                listBooks
-//            )
-//        }"
-//    )
-//}
+package lab1
+
+fun main() {
+    val books = """1. Shadow and Bone // Leigh Bardugo // 2012
+        2. City of Bones (Mortal Instruments) // Cassandra Clare // 2007
+        3. Ninth House // Leigh Bardugo // 2019
+        4. Dune // Franklin Patrick Herbert // 1965
+        5. Roadside Picnic // Arcady and Boris Strugataky // 1972"""
+    val listBooks: List<Book>? = parserBooks(books)
+    if (oldestBook(listBooks!!) != null) println("The one of the oldest book was published: ${oldestBook(listBooks)}")
+    if (youngestBook(listBooks) != null) println(
+        "The one of the youngest book was published: ${
+            youngestBook(
+                listBooks
+            )
+        }"
+    )
+    if (longestNameBook(listBooks) != null) println(
+        "The one of the longest name of the book is: ${
+            longestNameBook(
+                listBooks
+            )
+        }"
+    )
+    if (shortestNameBook(listBooks) != null) println(
+        "The one of the shortest name of the book is: ${
+            shortestNameBook(
+                listBooks
+            )
+        }"
+    )
+}
