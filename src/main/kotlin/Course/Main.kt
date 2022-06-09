b74aec40cbc6a73959c5687fb44115de25820aba
Alina <al.litovckina@yandex.ru>
2022-06-09 19:18:43 +0300

    couswork

--- src/main/kotlin/Course/Main.kt
+++ src/main/kotlin/Course/Main.kt
@@ -3,31 +3,12 @@ package lab1.Course
 import javax.swing.SwingUtilities
 
 fun main() {
+    val ff = forFile()
+    val list = ff.read()
 
-    val rr = Contact()
-    val tr = Contact()
-    val atr = Contact()
-    val arr = Contact()
-    val tt = mutableListOf<Contact>()
-    rr.changeFirstName("Vlad")
-    rr.changeIndex(3)
-    tr.changeFirstName("Alex")
-    tr.changeIndex(1)
-    arr.changeFirstName("Anna")
-    arr.changeDate("13.10.2001")
-    arr.changePhone("89112823272")
-    arr.changeAddress("Somewhere")
-    arr.changeEmail("arr@gmail.com")
-    atr.changeFirstName("Alina")
-    atr.changeIndex(2)
-    arr.changeSecondName("Rudnitskaya")
-    atr.changeSecondName("Reymhen")
-    tt.add(arr)
-    tt.add(tr)
-    tt.add(atr)
-    tt.add(rr)
     SwingUtilities.invokeLater {
-        val tableUi = TableUi(tt)
+        val tableUi = TableUi(list)
         tableUi.isVisible = true
    }
+
 }
--- src/main/kotlin/Course/TableUi.kt
+++ src/main/kotlin/Course/TableUi.kt
@@ -48,18 +48,12 @@ class TableUi(list: List<Contact>) : JFrame("Phone Book") {
         val fileName = ("vCard"+ind+".vcf")
         var file = File(fileName)
         FileOutputStream(file)
-        val person= workModel.contacts[ind].getAllInformation()
+        var person= workModel.contacts[ind].getAllInformation()
         File(fileName).appendText("BEGIN:VCARD \n")
         File(fileName).appendText("VERSION:3.0 \n")
-        File(fileName).appendText("$person")
+        File(fileName).appendText("${person}")
         File(fileName).appendText("END:VCARD \n")
     }
-
-//    fun readFromFile(): String {
-//        if (!File("AllInformation").exists())
-//            throw IllegalArgumentException("File doesn't exist")
-//        return File().readText()
-//    }
 
     private fun saveToVCardButton(ind:Int): Component{
         val saveVCard = JButton("Save in vCard-file")
@@ -100,8 +94,9 @@ class TableUi(list: List<Contact>) : JFrame("Phone Book") {
         val panel = JPanel().apply {
             if (flag) {add(deleteButton(ind)) }
             add(saveButton(flag, ind))
-            add(saveToVCardButton(ind))
             add(backButton())
+            add(saveToVCardButton(ind))
+
         }
         return panel
     }
@@ -110,17 +105,22 @@ class TableUi(list: List<Contact>) : JFrame("Phone Book") {
         val exit = JButton("Exit")
         updateFont(exit, 20.0f)
         exit.addActionListener{
-                val dialogOption = JOptionPane.showConfirmDialog(
-                    this,
-                    "You sure that you want to exit? All changes will be saved.",
-                    "Exit",
-                    JOptionPane.OK_CANCEL_OPTION
-                )
-
-                if (dialogOption == JOptionPane.OK_OPTION) {
-                    System.exit(0)
-                }
-
+            val ff = forFile()
+            var listAns = mutableListOf<Contact>()
+            var i =0
+            while(i < size){
+                var contact = Contact()
+                contact.changeFirstName(workModel.contacts[i].getFirstName())
+                contact.changeSecondName(workModel.contacts[i].getSecondName())
+                contact.changeDate(workModel.contacts[i].getDate())
+                contact.changeAddress(workModel.contacts[i].getAddress())
+                contact.changeEmail(workModel.contacts[i].getEmail())
+                contact.changePhone(workModel.contacts[i].getPhone())
+                listAns.add(contact)
+                i++
+            }
+            val list = ff.write(listAns)
+            System.exit(0)
         }
         return exit
     }
@@ -129,15 +129,6 @@ class TableUi(list: List<Contact>) : JFrame("Phone Book") {
         val detele = JButton("Delete")
         updateFont(detele, 20.0f)
         detele.addActionListener{
-            val dialogOption = JOptionPane.showConfirmDialog(
-                this,
-                "Delete the contact?",
-                "Delete",
-                JOptionPane.OK_CANCEL_OPTION
-            )
-
-            if (dialogOption == JOptionPane.OK_OPTION) {
-
             workModel.removeContact(workModel.contacts[ind])
             size--
             var i = 0
@@ -153,7 +144,6 @@ class TableUi(list: List<Contact>) : JFrame("Phone Book") {
             revalidate()
             repaint()
         }
-        }
         return detele
     }
 
@@ -161,38 +151,29 @@ class TableUi(list: List<Contact>) : JFrame("Phone Book") {
         val save = JButton("Save")
         updateFont(save, 20.0f)
         save.addActionListener{
-            val dialogOption = JOptionPane.showConfirmDialog(
-                this,
-                "Save the contact?",
-                "Save",
-                JOptionPane.OK_CANCEL_OPTION
-            )
-
-            if (dialogOption == JOptionPane.OK_OPTION) {
             if (!flag) {
                 val cont = Contact()
                 cont.changeFirstName(listContact[0].text)
                 cont.changeSecondName(listContact[1].text)
-                cont.changeDate(listContact[2].text)
+                cont.changePhone(listContact[2].text)
                 cont.changeAddress(listContact[3].text)
                 cont.changeEmail(listContact[4].text)
-                cont.changePhone(listContact[2].text)
+                cont.changeDate(listContact[5].text)
                 cont.changeIndex(size)
                 workModel.createContact(cont)
                 size++
             }    else {
                 workModel.contacts[ind].changeFirstName(listContact[0].text)
                 workModel.contacts[ind].changeSecondName(listContact[1].text)
-                workModel.contacts[ind].changeDate(listContact[2].text)
+                workModel.contacts[ind].changePhone(listContact[2].text)
                 workModel.contacts[ind].changeAddress(listContact[3].text)
                 workModel.contacts[ind].changeEmail(listContact[4].text)
-                workModel.contacts[ind].changePhone(listContact[5].text)
+                workModel.contacts[ind].changeDate(listContact[5].text)
             }
 //            listContact.clear()
 //            rootPane.contentPane = resubscribe() as Container?
 //            revalidate()
 //            repaint()
-        }
         }
         return save
     }
@@ -229,29 +210,29 @@ private fun createBoardPanel(): Component {
         listContact.add(JTextField(temp))
         temp = contact.getSecondName()
         listContact.add(JTextField(temp))
-        temp = contact.getDate().toString()
+        temp = contact.getPhone()
+        listContact.add(JTextField(temp))
+        temp = contact.getDate()
         listContact.add(JTextField(temp))
         temp = contact.getAddress()
         listContact.add(JTextField(temp))
         temp = contact.getEmail()
-        listContact.add(JTextField(temp))
-        temp = contact.getPhone()
         listContact.add(JTextField(temp))
         val panel = JPanel().apply {
             add(JLabel("Name:"))
             add(listContact[0])
             add(JLabel("Surname:"))
             add(listContact[1])
-            add(JLabel("Date of birth:"))
+            add(JLabel("Phone number:"))
             add(listContact[2])
             add(JLabel("Address:"))
             add(listContact[3])
             add(JLabel("Emails:"))
             add(listContact[4])
-            add(JLabel("Phone number:"))
+            add(JLabel("Date of birth:"))
             add(listContact[5])
         }
-        panel.layout = BoxLayout(panel, BoxLayout.PAGE_AXIS)
+        panel.layout = BoxLayout(panel, BoxLayout.Y_AXIS)
         return JScrollPane(panel)
     }
 
